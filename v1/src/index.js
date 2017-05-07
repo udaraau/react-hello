89e22a96c3a0ef83ce3bc86b29ad8fa37141aae8
Udara Bentota <udara.bentota@verifone.com>
2017-05-07 01:16:52 +0100

    jsx basic syntax test

--- v1/src/index.js
+++ v1/src/index.js
@@ -3,7 +3,26 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const user = {
+  firstName : 'Anishka',
+  secondName : 'Udara'
+}
+
+const e1 = (<h1> Hello, {getName(user)}</h1>);
+const e2 = (getGreeting(null));
+
+function getName(user) {
+  return user.firstName + " " + user.secondName;
+}
+
+function getGreeting(user) {
+  if(user) {
+    return (<h1> Hello, {getName(user)}</h1>);
+  }
+  return <h1> Hello, Stranger </h1>;
+}
+
 ReactDOM.render(
-  <App />,
+  e2,
   document.getElementById('root')
 );
